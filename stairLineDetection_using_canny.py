6c108ec6cbfd607ad0b688fc17aa501fc9083549
Sjeongheon <fight0105@gmail.com>
2024-07-14 20:09:04 +0900

    Feat : stair line detection with canny indoor stair image를 통한 edge 검출 noise가 너무 많음..

--- stairLineDetection_using_canny.py
+++ stairLineDetection_using_canny.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def detect_edges(image, low_threshold=70, high_threshold=210):
+def detect_edges(image, low_threshold=70, high_threshold=140):
     blur = cv2.GaussianBlur(image, (5,5), 0)
     return cv2.Canny(blur, low_threshold, high_threshold)
 
@@ -15,21 +15,21 @@ def detect_horizontal_lines(edges, rho=1, theta=np.pi/180, threshold=30, min_lin
                 horizontal_lines.append((x1, y1, x2, y2))
     return horizontal_lines
 
-def filter_edges_by_depth(image_lines, depth_image, min_depth, max_depth):
+def filter_edges_by_depth(image_lines, depth_lines, tolerance=10):
     filtered_lines = []
-    for line in image_lines:
-        x1, y1, x2, y2 = line
-        depth_values = depth_image[y1:y2, x1:x2] if y1 != y2 else depth_image[y1, x1:x2]
-        if np.any((depth_values >= min_depth) & (depth_values <= max_depth)):
-            filtered_lines.append(line)
+    for img_line in image_lines:
+        for dpt_line in depth_lines:
+            if abs(img_line[1] - dpt_line[1]) < tolerance:  # 수평 에지의 y값을 비교하여 유사한 에지를 필터링
+                filtered_lines.append(img_line)
+                break
     return filtered_lines
+  
 
 if __name__ == "__main__":
     # 이미지 경로 설정
-    image_path = './images/stair2.jpg'
+    image_path = './images/stair3.jpg'
     depth_image_path = './images/stair2_depth.png'
-
-    # 이미지 및 depth 이미지 읽기
+      # 이미지 및 depth 이미지 읽기
     image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
     depth_image = cv2.imread(depth_image_path, cv2.IMREAD_GRAYSCALE)
 
@@ -38,19 +38,25 @@ if __name__ == "__main__":
 
     # Edge Detection
     edges = detect_edges(image)
+    depth_edges = detect_edges(depth_image)
 
     # 수평 에지 검출
     image_lines = detect_horizontal_lines(edges)
+    depth_lines = detect_horizontal_lines(depth_edges)
 
-    # Depth 이미지 기반 필터링
-    min_depth = 50  # 적절한 최소 깊이 값 설정
-    max_depth = 200  # 적절한 최대 깊이 값 설정
-    filtered_lines = filter_edges_by_depth(image_lines, depth_image, min_depth, max_depth)
+    # Depth 에지를 사용하여 이미지 에지 필터링
+    filtered_lines = filter_edges_by_depth(image_lines, depth_lines)
 
     # 필터링된 에지를 원본 이미지에 그리기
     result_image = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
     for x1, y1, x2, y2 in filtered_lines:
-        cv2.line(result_image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv2.line(result_image, (x1, y1), (x2, y2), (0, 255, 0), 20)
+
+    for x1, y1, x2, y2 in image_lines:
+        cv2.line(result_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+    
+    for x1, y1, x2, y2 in depth_lines:
+        cv2.line(result_image, (x1, y1), (x2, y2), (0, 0, 255), 10)
 
     # Matplotlib을 사용하여 결과 시각화
     fig, axs = plt.subplots(2, 2, figsize=(15, 10))
